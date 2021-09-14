71263c5a0a47daa2db9f0624eb1622d6badd81b9
Antti Kantero <antti.kantero@outlook.com>
2021-09-14 11:31:18 +0300

    Lopullinen. Testi on on mielenkiitoinen: hashin poisto ja silti toimii

--- Password_cracker/cracker.py
+++ Password_cracker/cracker.py
@@ -1,31 +1,11 @@
-import hashlib
-from urllib.request import urlopen
- 
-def readwordlist(url):
-    try:
-        wordlistfile = urlopen(url).read()
-    except Exception as e:
-        print("Hey there was some error while reading the wordlist, error:", e)
-        exit()
-    return wordlistfile
- 
- 
-def hash(wordlistpassword):
-    result = hashlib.sha1(wordlistpassword.encode())
-    return result.hexdigest()
- 
- 
-def bruteforce(guesspasswordlist, actual_password_hash):
-    for guess_password in guesspasswordlist:
-        if hash(guess_password) == actual_password_hash:
-            print("Hello! I've cracked your password! Your password is:", guess_password,
-                  "\n please change this, it was really easy to guess it (:")
-            # If the password is found then it will terminate the script here
-            exit()
- 
+##### kaikki viety engine.py
+
+from engine import hash, readwordlist, bruteforce
+
+
 
 url = 'https://raw.githubusercontent.com/berzerk0/Probable-Wordlists/master/Real-Passwords/Top12Thousand-probable-v2.txt'
-actual_password = 'hackerit'
+actual_password = 'hacker'
 actual_password_hash = hash(actual_password)
  
 wordlist = readwordlist(url).decode('UTF-8')
---
+++ Password_cracker/engine.py
@@ -0,0 +1,26 @@
+import hashlib
+from urllib.request import urlopen
+
+ 
+def readwordlist(url):
+    try:
+        wordlistfile = urlopen(url).read()
+    except Exception as e:
+        print("Hey there was some error while reading the wordlist, error:", e)
+        exit()
+    return wordlistfile
+ 
+ 
+def hash(wordlistpassword):
+    result = hashlib.sha1(wordlistpassword.encode())
+    return result.hexdigest()
+ 
+ 
+def bruteforce(guesspasswordlist, actual_password_hash):
+    for guess_password in guesspasswordlist:
+        if hash(guess_password) == actual_password_hash:
+            print("Hello! I've cracked your password! Your password is:", guess_password,
+                  "\n please change this, it was really easy to guess it (:")
+            # If the password is found then it will terminate the script here
+            exit()
+ 
