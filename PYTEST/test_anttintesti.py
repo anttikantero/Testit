c1e950b8b59052e706e36ae6abb1da0ba9cc17e4
Antti Kantero <antti.kantero@outlook.com>
2021-09-15 16:21:24 +0300

    Ok

--- PYTEST/test_anttintesti.py
+++ PYTEST/test_anttintesti.py
@@ -2,10 +2,10 @@
 
 import pytest
 def test_file1_method1():
-	x=5
-	y=6
+	x=100
+	y=101
 	assert x+1 == y,"Testi epäonnistui"
 def test_file1_method2():
-	x=5
-	y=20
+	x=10
+	y=10
 	assert x*y == 100,"Testi epäonnistui" 
